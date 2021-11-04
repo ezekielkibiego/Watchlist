314b985c265a4247a81f5fab985268445615e4ca
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-11-04 09:05:09 +0300

    deployment to heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,40 +1,57 @@
-
 from flask import Flask
-from flask_bootstrap import Bootstrap
 from config import config_options
-
+from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
-
 from flask_login import LoginManager
+from flask_mail import Mail
+
+
+
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
-
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+mail = Mail()
+
+
+
+
 
 def create_app(config_name):
+
     app = Flask(__name__)
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+    # config_options[config_name].init_app(app)
 
 
-   #Initializing Flask Extensions
+
+    # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
+
     
+
+
+
+
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+
     # setting config
     from .request import configure_request
     configure_request(app)
+
 
     return app
